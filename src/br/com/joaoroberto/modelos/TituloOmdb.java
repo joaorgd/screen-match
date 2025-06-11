de092631e47180f9dc260933fe37ab725098a376
João Roberto <joaorobertoguedesdantas@gmail.com>
2025-06-11 13:00:56 -0300

    criação de classe record e integração com a biblioteca gson

---
+++ src/br/com/joaoroberto/modelos/TituloOmdb.java
@@ -0,0 +1,4 @@
+package br.com.joaoroberto.modelos;
+
+public record TituloOmdb(String title, String year,String runtime) {
+}
--- src/br/com/joaoroberto/principal/PrincipalComBusca.java
+++ src/br/com/joaoroberto/principal/PrincipalComBusca.java
@@ -1,20 +1,41 @@
 package br.com.joaoroberto.principal;
 
+import br.com.joaoroberto.modelos.Titulo;
+import br.com.joaoroberto.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura =  new Scanner(System.in);
+        System.out.print("Digite um filme: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=2d9a4818";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=fight+club&apikey=2d9a4818"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        //Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
 
     }
 }
